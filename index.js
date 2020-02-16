59117d283d69b91627e0676b27ca4bfabc067615
krocon@gmail.com <1git_15>
2020-02-16 16:09:09 +0100

    es6

--- index.js
+++ index.js
@@ -1,214 +1,224 @@
-(function () {
-    "use strict";
+"use strict";
 
-    module.exports = function (dir, options, callback) {
+import fs from "fs";
+import path from "path";
 
-        var fs = require('fs');
-        var path = require('path');
+import walk from "walkdir";
+import md5File from "md5-file";
+import log from "npmlog";
 
-        var walk = require('walkdir');
-        var md5File = require("md5-file");
-        var log = require('npmlog');
+export function findDuplicateFiles(dir, options, callback) {
 
-        if (!dir) return log.error('fdf', 'Parameter dir is missing');
+  if (!dir) {
+    return error('fdf', 'Parameter dir is missing');
+  }
 
-        var isArray = Array.isArray || function (obj) {
-            return toString.call(obj) === "[object Array]";
-        };
-        var isString = function (obj) {
-            return toString.call(obj) === "[object String]";
-        };
+  if (!options) options = {};
+  options.pathes = Array.isArray(dir) ? dir : [dir]; // options.pathes is an array!
+  options.callback = callback;
 
-        if (!options) options = {};
-        options.pathes = isArray(dir) ? dir : [dir]; // options.pathes is an array!
-        options.callback = callback;
+  // Check:
+  for (let i; i < options.pathes.length; i++) {
+    if (!isString(options.pathes[i])) {
+      return error('fdf', 'Parameter dir must be a string or an array of strings.');
+    }
+  }
 
-        // Check:
-        for (var i; i < options.pathes.length; i++) {
-            if (!isString(options.pathes[i])) return log.error('fdf', 'Parameter dir must be a string or an array of strings.');
+  start();
+
+  // ---------------------------------------------------
+
+  function isString(o) {
+    return typeof o === 'string';
+  }
+
+  function info(args) {
+    if (!options.silent) log.info(...args);
+  }
+
+
+  function error(args) {
+    log.error(...args);
+  }
+
+  function defaultCallback(err, groups) {
+    if (err) {
+      error("fdf", err);
+    } else {
+      groups.forEach(group => {
+        info("fdf", '');
+        group.forEach(item => {
+          info("fdf", item.path);
+        });
+      });
+    }
+  }
+
+  function containsFileItemByPath(list, path) {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].path === path) return true;
+    }
+    return false;
+  }
+
+  function getAllFiles(pathes) {
+    const files = [];
+    for (let i = 0; i < pathes.length; i++) {
+      const ff = getFiles(pathes[i]);
+      for (let j = 0; j < ff.length; j++) {
+        // We dont want to have duplicates in our file array:
+        if (files.indexOf(ff[j]) === -1) files.push(ff[j]);
+      }
+    }
+    return files;
+  }
+
+  function getFiles(dir) {
+    // We want get files only (no directories):
+    const files = [];
+    try {
+      let filesObject = walk.sync(dir, {
+        "return_object": true,
+        "no_return": false
+      });
+      for (let file in filesObject) {
+        if (filesObject.hasOwnProperty(file) && filesObject[file]["mode"] & 0x8000) files.push(file);
+      }
+    } catch (e) {
+      error('fdf', 'Error reading %s', dir, e);
+    }
+    return files;
+  }
+
+  function loadMd5File() {
+    let ret = [];
+    if (!options.md5SkipLoading) {
+      try {
+        ret = JSON.parse(fs.readFileSync(options.md5File));
+        info('fdf', 'File loaded   : %s', options.md5File);
+      } catch (e) {
+        info('fdf', 'Can\'t load %s (%s)', options.md5File, e.message);
+      }
+    }
+    return ret;
+  }
+
+  function saveMd5File(md5List) {
+    if (!options.md5SkipSaving) {
+      fs.writeFile(options.md5File, JSON.stringify(md5List) /*, null, 2)*/, err => {
+        if (err) return console.error(err);
+        info('fdf', 'JSON (md5) saved at : %s', options.md5File);
+      });
+    }
+  }
+
+  function removeOutdatedMd5Entries(files, md5List) {
+    // remove outdated entries from md5List:
+    if (md5List.length) {
+      for (let i = 0; i < md5List.length; i++) {
+        if (files.indexOf(md5List[i].path) === -1) {
+          md5List.splice(i, 1); // remove item from md5List
         }
+      }
+    }
+  }
+
+  function createMissingMd5(files, md5List) {
+    const t2 = Date.now();
+    for (let i = 0; i < files.length; i++) {
+      // Logging:
+      if (!options.silent && !options.silent && i > 0 && i % 1000 === 0) {
+        const dt = (Date.now() - t2);
+        const eta = Math.max(0, (dt * files.length / i - dt) / 1000).toFixed(0);
+        info('fdf', '%s %% (%d files) in %d millis (ETA: %s secs).', (100 * i / files.length).toFixed(1), i, dt, eta);
+      }
+      const p = files[i];
+      if (!options.checkPattern || p.match(options.checkPattern)) {
+        if (!md5List.length || !containsFileItemByPath(md5List, p)) {
+          const item = {path: p};
+          try {
+            item.md5 = md5File(p);
+            md5List.push(item);
+          } catch (e) {
+            error('fdf', '%d) %s (%s)', i, p, e.message);
+          }
+        }
+      }
+    } // for
+  }
+
+  function findDuplicates(md5List) {
+    const dublicatMd5s = [];
+    let fileCount = 0;
+    const ret = [];
+    for (let k = 0; k < md5List.length; k++) {
+      const foundDublicates = [];
+      const item1 = md5List[k];
+
+      for (let l = (k + 1); l < md5List.length; l++) {
+        const item2 = md5List[l];
+        if (item1.md5 === item2.md5) {
+          if (dublicatMd5s.indexOf(item1.md5) === -1) {
+            if (!containsFileItemByPath(foundDublicates, item1.path)) {
+              foundDublicates.push(item1);
+            }
+            if (!containsFileItemByPath(foundDublicates, item2.path)) {
+              foundDublicates.push(item2);
+            }
+          }
+        }
+      }
+      if (foundDublicates.length) {
+        ret.push(foundDublicates);
+        fileCount += foundDublicates.length;
+        dublicatMd5s.push(item1.md5);
+      }
+    }
+    info('fdf', 'Dublicates          : %d files in %d groups.', fileCount, ret.length);
+    return ret;
+  }
+
+  function start() {
+    // Starting...
+
+    if (!options.md5File) options.md5File = path.join(options.pathes[0], '/', 'md5.json');
+    options.callback = options.callback ? options.callback : defaultCallback;
+
+    info('fdf', 'Starting at   : %s', options.pathes);
+    info('fdf', 'Searching for : %s', options.checkPattern);
+    info('fdf', 'Start dirs    : %s', options.pathes);
+
+    // Scan directories recursive:
+    const t1 = Date.now();
+    const files = getAllFiles(options.pathes);
+    const t2 = Date.now();
+    info('fdf',
+      'Directory scan: %d entries (in %d start directories) in %d millis',
+      files.length, options.pathes.length, (t2 - t1));
+
+    // Load md5 file or create empty array:
+    const md5List = loadMd5File();
+
+    // remove outdated entries from md5List:
+    if (md5List.length) removeOutdatedMd5Entries(files, md5List);
+
+    // get md5 for each file in array 'files':
+    createMissingMd5(files, md5List);
+
+    const t3 = Date.now();
+    // Logging:
+    info('fdf', 'Creation of md5 list: %d entries in %d millis', md5List.length, (t3 - t2));
+
+    // Write file/md5 list to file:
+    saveMd5File(md5List);
+
+    // find duplicates by md5:
+    const ret = findDuplicates(md5List);
+
+    // call the caller:
+    options.callback(null, ret);
+  }
+
+}
 
 
-        (function (opt) {
-
-            var defaultCallback = function defaultCallback(err, groups) {
-                if (err) {
-                    log.error("fdf", err);
-                } else {
-                    groups.forEach(function (group) {
-                        log.info("fdf", '');
-                        group.forEach(function (item) {
-                            log.info("fdf", item.path);
-                        });
-                    });
-                }
-            };
-
-            var containsFileItemByPath = function containsFileItemByPath(list, path) {
-                for (var i = 0; i < list.length; i++) {
-                    if (list[i].path === path) return true;
-                }
-                return false;
-            };
-
-            var getAllFiles = function getAllFiles(pathes) {
-                var files = [];
-                for (var i = 0; i < pathes.length; i++) {
-                    var ff = getFiles(pathes[i]);
-                    for (var j = 0; j < ff.length; j++) {
-                        // We dont want to have duplicates in our file array:
-                        if (files.indexOf(ff[j]) === -1) files.push(ff[j]);
-                    }
-                }
-                return files;
-            };
-            var getFiles = function getFiles(dir) {
-                // We want get files only (no directories):
-                var files = [];
-                try {
-                    var filesObject = walk.sync(dir, {
-                        "return_object": true,
-                        "no_return": false
-                    });
-                } catch (e) {
-                    log.error('fdf', 'Error reading %s', dir, e);
-                }
-                for (var file in filesObject) {
-                    if (filesObject.hasOwnProperty(file) && filesObject[file]["mode"] & 0x8000) files.push(file);
-                }
-                return files;
-            };
-
-            var loadMd5File = function loadMd5File() {
-                var ret = [];
-                if (!opt.md5SkipLoading) {
-                    try {
-                        ret = JSON.parse(fs.readFileSync(opt.md5File));
-                        if (!opt.silent) log.info('fdf', 'File loaded   : %s', opt.md5File);
-                    } catch (e) {
-                        log.info('fdf', 'Can\'t load %s (%s)', opt.md5File, e.message);
-                    }
-                }
-                return ret;
-            };
-
-            var saveMd5File = function saveMd5File(md5List) {
-                if (!opt.md5SkipSaving) {
-                    fs.writeFile(opt.md5File, JSON.stringify(md5List) /*, null, 2)*/, function (err) {
-                        if (err) return console.error(err);
-                        if (!opt.silent) log.info('fdf', 'JSON (md5) saved at : %s', opt.md5File);
-                    });
-                }
-            };
-
-            var removeOutdatedMd5Entries = function removeOutdatedMd5Entries(files, md5List) {
-                // remove outdated entries from md5List:
-                if (md5List.length) {
-                    for (var i = 0; i < md5List.length; i++) {
-                        if (files.indexOf(md5List[i].path) === -1) {
-                            md5List.splice(i, 1); // remove item from md5List
-                        }
-                    }
-                }
-            };
-
-            var createMissingMd5 = function createMissingMd5(files, md5List) {
-                var t2 = Date.now();
-                for (var i = 0; i < files.length; i++) {
-                    // Logging:
-                    if (!opt.silent && !opt.silent && i > 0 && i % 1000 === 0) {
-                        var dt = (Date.now() - t2);
-                        var eta = Math.max(0, (dt * files.length / i - dt) / 1000).toFixed(0);
-                        log.info('fdf', '%s %% (%d files) in %d millis (ETA: %s secs).', (100 * i / files.length).toFixed(1), i, dt, eta);
-                    }
-                    var p = files[i];
-                    if (!opt.checkPattern || p.match(opt.checkPattern)) {
-                        if (!md5List.length || !containsFileItemByPath(md5List, p)) {
-                            var item = {path: p};
-                            try {
-                                item.md5 = md5File(p);
-                                md5List.push(item);
-                            } catch (e) {
-                                log.error('fdf', '%d) %s (%s)', i, p, e.message);
-                            }
-                        }
-                    }
-                } // for
-            };
-
-            var findDuplicates = function findDuplicates(md5List) {
-                var dublicatMd5s = [];
-                var fileCount = 0;
-                var ret = [];
-                for (var k = 0; k < md5List.length; k++) {
-                    var foundDublicates = [];
-                    var item1 = md5List[k];
-
-                    for (var l = (k + 1); l < md5List.length; l++) {
-                        var item2 = md5List[l];
-                        if (item1.md5 === item2.md5) {
-                            if (dublicatMd5s.indexOf(item1.md5) === -1) {
-                                if (!containsFileItemByPath(foundDublicates, item1.path)) foundDublicates.push(item1);
-                                if (!containsFileItemByPath(foundDublicates, item2.path)) foundDublicates.push(item2);
-                            }
-                        }
-                    }
-                    if (foundDublicates.length) {
-                        ret.push(foundDublicates);
-                        fileCount += foundDublicates.length;
-                        dublicatMd5s.push(item1.md5);
-                    }
-                }
-                if (!opt.silent) log.info('fdf', 'Dublicates          : %d files in %d groups.', fileCount, ret.length);
-                return ret;
-            };
-
-
-            var start = function start() {
-                // Starting...
-
-                if (!opt.md5File) opt.md5File = path.join(opt.pathes[0], '/', 'md5.json');
-                opt.callback = opt.callback ? opt.callback : defaultCallback;
-
-                if (!opt.silent) {
-                    log.info('fdf', 'Starting at   : %s', opt.pathes);
-                    log.info('fdf', 'Searching for : %s', opt.checkPattern);
-                    log.info('fdf', 'Start dirs    : %s', opt.pathes);
-                }
-
-                // Scan directories recursive:
-                var t1 = Date.now();
-                var files = getAllFiles(opt.pathes);
-                var t2 = Date.now();
-                if (!opt.silent) log.info('fdf', 'Directory scan: %d entries (in %d start directories) in %d millis', files.length, opt.pathes.length, (t2 - t1));
-
-                // Load md5 file or create empty array:
-                var md5List = loadMd5File();
-
-                // remove outdated entries from md5List:
-                if (md5List.length) removeOutdatedMd5Entries(files, md5List);
-
-                // get md5 for each file in array 'files':
-                createMissingMd5(files, md5List);
-
-                var t3 = Date.now();
-                // Logging:
-                if (!opt.silent) log.info('fdf', 'Creation of md5 list: %d entries in %d millis', md5List.length, (t3 - t2));
-
-                // Write file/md5 list to file:
-                saveMd5File(md5List);
-
-                // find duplicates by md5:
-                var ret = findDuplicates(md5List);
-
-                // call the caller:
-                opt.callback(null, ret);
-            };
-
-            start();
-
-        })(options);
-
-    }; // module.exports
-
-})();
--- test/test.js
+++ test/test.js
@@ -1,149 +1,145 @@
-(function(){
-    
-    'use strict';
-    
-    // be carefull that your ide doesnt change the input of the test files (line breaks).
+'use strict';
 
-    var fs = require('fs');
-    var findDuplicateFiles = require('../index.js'); // find-duplicate-files
-    var assert = require('assert');
-    var Promise = require('es6-promise').Promise;
+// be carefull that your ide doesnt change the input of the test files (line breaks).
 
-    function deleteMd5(){
-        return new Promise(function(resolve, reject) {
-            fs.stat('da/md5.json', function (err, stats) {
-                if (stats && stats.isFile()) {
-                    fs.unlink('da/md5.json', function () {
-                        resolve();
-                    });
-                } else {
-                    resolve();
-                }
-            });
+import fs from 'fs';
+import {findDuplicateFiles} from '../index.js'; // find-duplicate-files
+import assert from 'assert';
+
+
+function deleteMd5() {
+  return new Promise((resolve, reject) => {
+    fs.stat('da/md5.json', (err, stats) => {
+      if (stats && stats.isFile()) {
+        fs.unlink('da/md5.json', () => {
+          resolve();
         });
-    }
+      } else {
+        resolve();
+      }
+    });
+  });
+}
 
 
+function callTest1() {
+  console.info('Start 1...');
+  return new Promise((resolve, reject) => {
+    findDuplicateFiles('da', {silent: true},
+      (err, groups) => {
+        assert(err === null);
+        assert(groups.length === 2);
+        assert(groups[0].length === 8);
+        assert(groups[1].length === 4);
+        console.info('Path 1/6');
+        resolve();
+      }
+    );
+  });
+}
 
-    function callTest1(){
-        console.info('Start 1...');
-        return new Promise(function(resolve, reject) {
-            findDuplicateFiles('da', { silent: true },
-                function (err, groups) {
-                    assert(err === null);
-                    assert(groups.length == 2);
-                    assert(groups[0].length == 8);
-                    assert(groups[1].length == 4);
-                    console.info('Path 1/6');
-                    resolve();
-                }
-            );
-        });
-    }
+function callTest2() {
+  console.info('Start 2...');
+  return new Promise((resolve, reject) => {
+    findDuplicateFiles('da', {silent: true},
+      (err, groups) => {
+        assert(err === null);
+        assert(groups.length === 2);
+        assert(groups[0].length === 8);
+        assert(groups[1].length === 4);
+        console.info('Path 2/6');
+        resolve();
+      }
+    );
+  });
+}
 
-    function callTest2(){
-        console.info('Start 2...');
-        return new Promise(function(resolve, reject) {
-            findDuplicateFiles('da', { silent: true },
-                function (err, groups) {
-                    assert(err === null);
-                    assert(groups.length == 2);
-                    assert(groups[0].length == 8);
-                    assert(groups[1].length == 4);
-                    console.info('Path 2/6');
-                    resolve();
-                }
-            );
-        });
-    }
+function callTest3() {
+  console.info('Start 3...');
+  return new Promise((resolve, reject) => {
+    findDuplicateFiles('da', {silent: true, checkPattern: /\.dummy$/g,},
+      (err, groups) => {
+        assert(err === null);
+        assert(groups.length === 0);
+        console.info('Path 3/6');
+        resolve();
+      }
+    );
+  });
+}
 
-    function callTest3(){
-        console.info('Start 3...');
-        return new Promise(function(resolve, reject) {
-            findDuplicateFiles('da', { silent: true, checkPattern: /\.dummy$/g,  },
-                function (err, groups) {
-                    assert(err === null);
-                    assert(groups.length == 0);
-                    console.info('Path 3/6');
-                    resolve();
-                }
-            );
-        });
-    }
+function callTest4() {
+  console.info('Start 4...');
+  return new Promise((resolve, reject) => {
+    findDuplicateFiles('da', {
+        checkPattern: /\.js$|\.doc$/g, // can be null
+        md5SkipSaving: true,
+        md5SkipLoading: true,
+        silent: true
+      },
 
-    function callTest4(){
-        console.info('Start 4...');
-        return new Promise(function(resolve, reject) {
-            findDuplicateFiles('da', {
-                    checkPattern: /\.js$|\.doc$/g, // can be null
-                    md5SkipSaving: true,
-                    md5SkipLoading: true,
-                    silent: true
-                },
+      (err, groups) => {
+        assert(err === null);
+        assert(groups.length === 1);
+        assert(groups[0].length === 4);
 
-                function (err, groups) {
-                    assert(err === null);
-                    assert(groups.length == 1);
-                    assert(groups[0].length == 4);
+        console.info('Path 4/6');
+        resolve();
+      }
+    );
+  });
+}
 
-                    console.info('Path 4/6');
-                    resolve();
-                }
-            );
-        });
-    }
+function callTest5() {
+  console.info('Start 5...');
+  return new Promise((resolve, reject) => {
+    findDuplicateFiles('da', {
+        checkPattern: /\.dummytetetetet$/g,
+        md5SkipSaving: true,
+        md5SkipLoading: true,
+        silent: true
+      },
 
-    function callTest5(){
-        console.info('Start 5...');
-        return new Promise(function(resolve, reject) {
-            findDuplicateFiles('da', {
-                    checkPattern: /\.dummytetetetet$/g,
-                    md5SkipSaving: true,
-                    md5SkipLoading: true,
-                    silent: true
-                },
-
-                function (err, groups) {
-                    assert(err === null);
-                    assert(groups.length == 0);
-                    console.info('Path 5/6');
-                    resolve();
-                }
-            );
-        });
-    }
+      (err, groups) => {
+        assert(err === null);
+        assert(groups.length === 0);
+        console.info('Path 5/6');
+        resolve();
+      }
+    );
+  });
+}
 
 
-    function callTest6(){
-        console.info('Start 6...');
-        return new Promise(
-            function(resolve, reject) {
-                findDuplicateFiles(['da', 'dd'], {
-                    silent: true,
-                    md5SkipSaving: true,
-                    md5SkipLoading: true
-                },
-                function (err, groups) {
-                    assert(err === null);
-                    assert(groups.length == 2);
-                    assert(groups[0].length == 12);
-                    assert(groups[1].length == 8);
-                    console.info('Path 6/6');
-                    resolve();
-                }
-            );
-        });
-    }
+function callTest6() {
+  console.info('Start 6...');
+  return new Promise(
+    (resolve, reject) => {
+      findDuplicateFiles(['da', 'dd'], {
+          silent: true,
+          md5SkipSaving: true,
+          md5SkipLoading: true
+        },
+        (err, groups) => {
+          assert(err === null);
+          assert(groups.length === 2);
+          assert(groups[0].length === 12);
+          assert(groups[1].length === 8);
+          console.info('Path 6/6');
+          resolve();
+        }
+      );
+    });
+}
 
-    deleteMd5()
-        .then(callTest1)
-        .then(callTest2)
-        .then(deleteMd5)
-        .then(callTest3)
-        .then(callTest4)
-        .then(callTest5)
-        .then(deleteMd5)
-        .then(callTest6)
-        .then(deleteMd5);
+deleteMd5()
+  .then(callTest1)
+  .then(callTest2)
+  .then(deleteMd5)
+  .then(callTest3)
+  .then(callTest4)
+  .then(callTest5)
+  .then(deleteMd5)
+  .then(callTest6)
+  .then(deleteMd5);
 
-})();
